66ecc55ff798885f94624c5e6652dcdb889e8f59
Mike <mikekh@uw.edu>
2016-03-24 19:50:57 -0700

    a couple of tests passing for password/username

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -5,21 +5,24 @@ from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.security import (
     Allow,
     Everyone,
-    )
+    Authenticated,)
 
 from sqlalchemy import engine_from_config
 from .models import (
     DBSession,
-    Base, 
+    Base,
 )
 
+
 class MyRoot(object):
-    __acl__  = [(Allow, Everyone, 'secured')]
+    """Custom Root."""
+    __acl__  = [(Allow, Authenticated, 'secured')]
 
     def __init__(self, request):
+        """Init."""
         self.request = request
 
- 
+
 def main(global_config, **settings):
     """Function returns a Pyramid WSGI application."""
     engine = engine_from_config(settings, 'sqlalchemy.')
@@ -40,9 +43,9 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('secure', '/secure')
     config.add_route('entry', '/entry/{id:\d+}')
     config.add_route('new', '/new')
     config.add_route('edit', '/edit/{id:\d+}')
-    config.add_route('secure', '/secure')
     config.scan()
     return config.make_wsgi_app()
--- testapp/tests/test_auth.py
+++ testapp/tests/test_auth.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 """Test file for views."""
+import os
 import pytest
 import webtest 
 from testapp import main
@@ -10,6 +11,11 @@ def app():
     app = main({}, **settings)
     return webtest.TestApp(app)
 
+@pytest.fixture()
+def auth_env():
+    os.environ['AUTH_PASSWORD'] = 'secret'
+    os.environ['AUTH_USERNAME'] = 'admin'
+
 def test_no_access_to_view(app):
     response = app.get('/secure', status=403)
     assert response.status_code == 403
@@ -17,3 +23,9 @@ def test_no_access_to_view(app):
 def test_access_to_view(app):
     response = app.get('/secure')
     assert response.status_code == 200
+
+def test_password_exsists(auth_env):
+    assert os.environ.get('AUTH_PASSWORD', None) is not None
+ 
+def test_username_exsists(auth_env):
+    assert os.environ.get('AUTH_USERNAME', None) is not None

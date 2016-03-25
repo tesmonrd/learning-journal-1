03b7007dc091eb93fe19337d9c455e714295790c
Mike <mikekh@uw.edu>
2016-03-24 21:46:30 -0700

    hashed pw and test

--- testapp/security.py
+++ testapp/security.py
@@ -1,4 +1,6 @@
 import os
+from passlib.apps import custom_app_context as pwd_context
 
 def check_pw(pw):
-    return pw == os.environ.get('AUTH_PASSWORD', 'this is not a password')
+    hashed = os.environ.get('AUTH_PASSWORD', 'this is not a password')
+    return pwd_context.verify(pw, hashed)
--- testapp/tests/test_auth.py
+++ testapp/tests/test_auth.py
@@ -13,19 +13,28 @@ def app():
 
 @pytest.fixture()
 def auth_env():
-    os.environ['AUTH_PASSWORD'] = 'secret'
+    from testapp.security import pwd_context
+    os.environ['AUTH_PASSWORD'] = pwd_context.encrypt('secret')
     os.environ['AUTH_USERNAME'] = 'admin'
 
 def test_no_access_to_view(app):
     response = app.get('/secure', status=403)
     assert response.status_code == 403
 
-def test_access_to_view(app):
-    response = app.get('/secure')
-    assert response.status_code == 200
+# def test_access_to_view(app):
+#     response = app.get('/secure')
+#     assert response.status_code == 200
 
 def test_password_exsists(auth_env):
     assert os.environ.get('AUTH_PASSWORD', None) is not None
  
 def test_username_exsists(auth_env):
     assert os.environ.get('AUTH_USERNAME', None) is not None
+
+def test_check_pw_success(auth_env):
+    from testapp.security import check_pw
+    password = 'secret'
+    assert check_pw(password)
+
+def test_stored_password_is_encrypted(auth_env):
+    assert os.environ.get('AUTH_PASSWORD', None) != 'secret'
